a2da9fb866671f3722696d2ce7a6ec6c3b709908
夏沫花火zzz🌙 <xmcn@1l1.icu>
2024-09-20 14:36:32 +0800

    Update footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,22 +1,13 @@
 <template>
   <v-footer>
-    <div class="footer-box">
-      <h2>Credits</h2>
-      <h3>
+    <div>
+      <p>
         开源协议
-        <a href="https://github.com/Muska-Ami/NyaLCF/blob/main/LICENSE" target="_blank"
-          ><v-icon icon="mdi-license"></v-icon>MIT</a
-        >
-      </h3>
-      <h3>贡献者</h3>
-      <router-link to="/credits">
-        <img src="https://contrib.rocks/image?repo=Muska-Ami/NyaLCF" height="32px" />
-      </router-link>
-      <br>
-      <router-link to="/credits">
-        <v-btn><v-icon icon="mdi-credit-card-chip"></v-icon>更多</v-btn>
-      </router-link>
+        <a href="https://github.com/Muska-Ami/NyaLCF/blob/main/LICENSE" target="_blank"> MIT </a>
+        |
+        <router-link to="/credits">Credits</router-link>
+      </p>
+      Copyright © 2023 夏沫花火zzz🌙.
     </div>
-    Copyright © 2023 夏沫花火zzz🌙.
   </v-footer>
 </template>
